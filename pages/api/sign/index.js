2fb06b03dc9c75164c7740e58f374e71d432a7b6
Yoseph Tamene <tameney22@mail.wlu.edu>
2023-01-23 00:16:26 -0500

    test

--- pages/api/sign/index.js
+++ pages/api/sign/index.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
   return new Promise((resolve, reject) => {
     pdf.create(renderedHtml, options).toBuffer(function (err, buff) {
       if (err) {
-        res.status(404).json({ error: err.message })
+        res.status(404).json({ error: err.message, renderedHtml })
       } else {
         res.setHeader('Content-Type', 'application/pdf');
         res.setHeader('Content-Disposition', 'attachment; filename=signed agreement.pdf');
@@ -25,3 +25,24 @@ export default async function handler(req, res) {
 
   })
 }
+
+
+// export default async function handler(req, res) {
+
+//   const templatesDir = path.join(process.cwd(), 'templates');
+//   var html = fs.readFileSync(templatesDir + '/toPdf.html', 'utf8');
+//   // var options = { format: 'A4', base: "http://" + req.headers.host, dpi: "300", };
+//   let options = { format: 'A4' };
+
+//   let renderedHtml = ejs.render(html, { form: req.body })
+
+//   htmlToPdf.generatePdf({ content: renderedHtml }, options).then(buff => {
+//     // if (err) {
+//     //   res.status(404).json({ error: err.message })
+//     // } else {
+//     res.setHeader('Content-Type', 'application/pdf');
+//     res.setHeader('Content-Disposition', 'attachment; filename=signed agreement.pdf');
+//     res.status(250).send(Buffer.from(buff));
+//   });
+
+// }
