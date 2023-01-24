e5d83be9fd3f7d2426fbffbe40c7bd26e3f68641
Yoseph Tamene <tameney22@mail.wlu.edu>
2023-01-23 23:31:37 -0500

    check

---
+++ pages/api/test.js
@@ -0,0 +1,56 @@
+import path from "path"
+import fs from "fs"
+import ejs from "ejs"
+
+export default async function handler(req, res) {
+  const templatesDir = path.join(process.cwd(), 'templates');
+  var html = fs.readFileSync(templatesDir + '/toPdf.html', 'utf8');
+  var options = { format: 'A4', base: "http://" + req.headers.host, dpi: "300", };
+
+  let renderedHtml = ejs.render(html, { form: req.body })
+
+  // const response = await fetch(
+  //   `https://api.html2pdf.app/v1/generate?html=https://example.com&apiKey=X77C4zkdv6qiqjAnc8oOj5vP68Figw6k7bQer5N4ArxrfbrE40UPkNuuSpyrB11X`,
+  //   // {
+  //   //   method: 'POST',
+  //   //   body: JSON.stringify({
+  //   //     html: renderedHtml,
+  //   //     apiKey: "X77C4zkdv6qiqjAnc8oOj5vP68Figw6k7bQer5N4ArxrfbrE40UPkNuuSpyrB11X"
+  //   //   }),
+  //   // }
+  // )
+
+  // const resJson = await response.data()
+
+  const response = await fetch(
+    `https://selectpdf.com/api2/convert/`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        key: '6a8128bf-eb03-474f-8a6a-7b420109b566',
+        // url: 'https://google.com'
+        html: renderedHtml,
+        "base_url": "https://tsionconsulting.netlify.app"
+      })
+    }
+  )
+
+  if (response.status == 200) {
+    const blob = await response.blob()
+    // const pdfBlob = new Blob([blob], { type: 'application/pdf' });
+    const arrBuff = await blob.arrayBuffer()
+    const buff = Buffer.from(arrBuff)
+
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename=signed agreement.pdf');
+    return res.status(250).send(Buffer.from(buff));
+  }
+
+
+  console.log("RESPONSE:", response)
+  // res.json({ status: response.status })
+  // res.send(response.data)
+}
