eb834484308d2c0763e11e2b72936dab734cd917
Yoseph Tamene <tameney22@mail.wlu.edu>
2023-01-24 01:17:03 -0500

    using json req.body

--- pages/api/test.js
+++ pages/api/test.js
@@ -8,6 +8,9 @@ export default async function handler(req, res) {
     'https://us-central1-tsion-consulting.cloudfunctions.net/helloHttp',
     {
       method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
       body: JSON.stringify(req.body),
     }
   )
