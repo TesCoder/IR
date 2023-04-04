a1a8c99549ab9886a496192f5e013523b69e3ff0
TesCoder <115577869+TesCoder@users.noreply.github.com>
2023-04-03 21:31:04 -0700

    contact update

--- next-js-site/pages/contact.js
+++ next-js-site/pages/contact.js
@@ -3,10 +3,7 @@ import ContactForm from "@/components/ContactForm";
 export default function Contact() {
   return (
     <>
-      <h1 className='font-medium md:font-light text-2xl md:text-4xl text-ivy-blue mb-3'>
-        Have any Questions?
-      </h1>
-      <p className='text-lg my-3 text-gray-700'>Please complete the form below for a prompt reply.</p>
+
       <ContactForm type="FULL" />
     </>
   )
