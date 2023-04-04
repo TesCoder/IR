c370f075f1cf8f4aa09fe815bfdccb52f259fe19
Yoseph Tamene <yosephtamene@Yosephs-MacBook-Air.local>
2023-04-04 01:55:07 -0400

    removed comments

--- next-js-site/components/layout.js
+++ next-js-site/components/layout.js
@@ -5,10 +5,7 @@ import Navbar from "./navbar";
 const noNavPages = ["/contact"];
 
 export default function Layout({ children }) {
-  // Get the pathname
   const { pathname } = useRouter();
-
-  // // Check if the current route matches a public page
   const hideNav = noNavPages.includes(pathname);
 
   return (
