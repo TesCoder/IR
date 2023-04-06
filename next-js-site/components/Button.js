0292e56bfb6ecc34a236464cbef4267319caba00
TesCoder <115577869+TesCoder@users.noreply.github.com>
2023-04-06 10:04:17 -0700

    netlify error, ran npm run build locall

--- next-js-site/components/Button.js
+++ next-js-site/components/Button.js
@@ -8,6 +8,7 @@ export function Button({ className, children, ...others }) {
   )
 }
 
+
 // ButtonRow function creates rows of 'Get in Touch', 'Get a Quick', 'Call now' buttons
 export const ButtonRow = ({ setModalType, lightBg }) => (
   <div className={`flex justify-center ${lightBg ? "" : "bg-gray-100"}`}>
