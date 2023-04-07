fca690f9a6134114bdb813505653334f727dbc05
TesCoder <115577869+TesCoder@users.noreply.github.com>
2023-04-07 08:15:18 -0700

    form submission conf changed to white

--- next-js-site/components/Alert.js
+++ next-js-site/components/Alert.js
@@ -3,7 +3,7 @@ export default function Alert({ success, message }) {
   if (!message) return
   return (
     <div className={`bg-ivy-red py-4 px-4 rounded-lg shadow-md text-white font-raleway`} role="alert" >
-      <h2 className="text-3xl font-semibold" >
+      <h2 className="text-3xl font-semibold text-white" >
         {/* <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Info:"><use xlink:href="#info-fill" /></svg> */}
         {/* {success ? "Form Submitted!" : "Form Error"} */}
         {/* tmporarily removed Form Error as it's doing so incorrectly & dont' want user to see.
@@ -11,9 +11,9 @@ export default function Alert({ success, message }) {
         {success ? "Form Submitted!" : "Form Submitted"}
       </h2 >
       <hr className="my-2" />
-      <p className="text-lg">{message}</p>
+      <p className="text-lg text-white">{message}</p>
       <br />
-      <p>- Ivy Ready</p>
+      <p className="text-white">- Ivy Ready</p>
     </div >
   )
 }
