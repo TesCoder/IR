445c93fa4abd040a100ad10c7a30b7f13d0bd369
TesCoder <115577869+TesCoder@users.noreply.github.com>
2023-04-10 11:28:54 -0700

    moved markee below banner

--- next-js-site/pages/index.js
+++ next-js-site/pages/index.js
@@ -75,6 +75,29 @@ export default function Home() {
         <ContactForm showProfile={true} coachName={coach} />
       </Modal>
 
+
+
+      {/* Main Banner */}
+      <div
+        className="flex bg-hero bg-cover bg-blue-300 pt-20 sm:py-72 md:py-72 lg:py-80 2xl:py-96"
+        style={{ height: "400px" }}
+      >
+        <div className="m-auto text-center">
+          <h1
+            className="text-ivy-red text-4xl lg:text-7xl "
+            style={{ textShadow: "5px 5px 5px white" }}
+          >
+            Ivy Ready College Consulting
+          </h1>
+          {/* <h4 className='text-2xl my-3 drop-shadow-lg italic font-medium'>
+            Personalized College Admission Support
+            <br />
+            by Former Admission Officers
+          </h4> */}
+        </div>
+      </div>
+
+      {/* Marquee */}
       <div className="flex justify-center">
         <div className="overflow-x-hidden w-5/6 md:w-3/3 ">
           <div className="py-3 animate-marquee whitespace-nowrap ">
@@ -102,26 +125,6 @@ export default function Home() {
             <MarqueeElement>Reach it</MarqueeElement>
             <MarqueeElement>〰️</MarqueeElement>
           </div>
-        </div>
-      </div>
-
-      {/* Main Banner */}
-      <div
-        className="flex bg-hero bg-cover bg-blue-300 pt-20 sm:py-72 md:py-72 lg:py-80 2xl:py-96"
-        style={{ height: "400px" }}
-      >
-        <div className="m-auto text-center">
-          <h1
-            className="text-ivy-red text-4xl lg:text-7xl "
-            style={{ textShadow: "5px 5px 5px white" }}
-          >
-            Ivy Ready College Consulting
-          </h1>
-          {/* <h4 className='text-2xl my-3 drop-shadow-lg italic font-medium'>
-            Personalized College Admission Support
-            <br />
-            by Former Admission Officers
-          </h4> */}
         </div>
       </div>
 
