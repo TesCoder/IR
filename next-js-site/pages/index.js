a6d0bda202afead175be46ab00454f8755b1828f
TesCoder <115577869+TesCoder@users.noreply.github.com>
2023-04-12 12:23:53 -0700

    changed banner bg to white

--- next-js-site/pages/index.js
+++ next-js-site/pages/index.js
@@ -77,7 +77,7 @@ export default function Home() {
 
       {/* Main Banner */}
       <div
-        className="flex bg-hero bg-cover bg-blue-300 pt-20 sm:py-72 md:py-72 lg:py-80 2xl:py-96"
+        className="flex bg-hero bg-cover bg-white pt-20 sm:py-72 md:py-72 lg:py-80 2xl:py-96"
         style={{ height: "400px" }}
       >
         <div className="m-auto text-center">
