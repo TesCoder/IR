c02a320df484deb9f51a514a682e7ea8f111d99c
TesCoder <115577869+TesCoder@users.noreply.github.com>
2023-05-02 15:29:23 -0700

    marquee module error trying to fix

--- next-js-site/components/MarqueeDisplay.js
+++ next-js-site/components/MarqueeDisplay.js
@@ -1,10 +1,8 @@
 {/* Main Marquee */}
-
-const MarqueeElement = ({ children }) => (
-  <span className="text-2xl mx-2 text-ivy-blue font-light">{...children}</span>
-);
-
 export default function MarqueeDisplay() {
+  const MarqueeElement = ({ children }) => (
+    <span className="text-2xl mx-2 text-ivy-blue font-light">{...children}</span>
+  );
     return (
       <div className="flex justify-center">
         <div className="overflow-x-hidden w-5/6 md:w-3/3 ">
