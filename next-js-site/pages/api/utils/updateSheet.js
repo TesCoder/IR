31f75c329e2a2396494c906c1ab3142d03a7bdf2
Yoseph Tamene <tameney22@mail.wlu.edu>
2024-01-15 21:19:47 -0500

    sheet per year

--- next-js-site/pages/api/utils/updateSheet.js
+++ next-js-site/pages/api/utils/updateSheet.js
@@ -1,32 +1,79 @@
-import { GoogleSpreadsheet } from 'google-spreadsheet';
+import { GoogleSpreadsheet } from "google-spreadsheet";
 
-const { SPREADSHEET_ID, GOOGLE_CLIENT_EMAIL, GOOGLE_SERVICE_PRIVATE_KEY } = process.env
+const { SPREADSHEET_ID, GOOGLE_CLIENT_EMAIL, GOOGLE_SERVICE_PRIVATE_KEY } =
+  process.env;
 
-const doc = new GoogleSpreadsheet(SPREADSHEET_ID)
-const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
+const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 
-const colNames = ["Date", "Time", "ConvType", "Stage", "StageDesc", "Email", "Phone", "FirstName", "LastName", "Prospect", "StudentYear", "ProvidedLocation", "ContactMethod", "Options", "InfoRequested", "ConvSource"]
+const colNames = [
+  "Date",
+  "Time",
+  "ConvType",
+  "Stage",
+  "StageDesc",
+  "Email",
+  "Phone",
+  "FirstName",
+  "LastName",
+  "Prospect",
+  "StudentYear",
+  "ProvidedLocation",
+  "ContactMethod",
+  "Options",
+  "InfoRequested",
+  "ConvSource",
+];
 
-export const updateSheet = async ({ id, coach, type, fname, lname, location, email, phone, year, contact, option, info, heard, service }) => {
-  console.log("REACHED UPDATE SHEET!")
+export const updateSheet = async ({
+  id,
+  coach,
+  type,
+  fname,
+  lname,
+  location,
+  email,
+  phone,
+  year,
+  contact,
+  option,
+  info,
+  heard,
+  service,
+}) => {
+  console.log("REACHED UPDATE SHEET!");
   try {
     await doc.useServiceAccountAuth({
       client_email: GOOGLE_CLIENT_EMAIL,
-      private_key: GOOGLE_SERVICE_PRIVATE_KEY
+      private_key: GOOGLE_SERVICE_PRIVATE_KEY,
     });
 
     await doc.loadInfo();
 
     let sheet = doc.sheetsByIndex[0];
     const today = new Date();
-    const month = monthNames[today.getMonth()];
-    const desiredTitle = `${month} ${today.getFullYear()}`
-    // If first sheet is from previous/other month create new sheet with this month's name 
+    // const month = monthNames[today.getMonth()];
+    const desiredTitle = `${today.getFullYear()}`;
+    // If first sheet is from previous/other month create new sheet with this month's name
     if (sheet.title != desiredTitle) {
-      sheet = await doc.addSheet({ title: desiredTitle, headerValues: colNames });
-      await sheet.updateProperties({ index: 0 })
+      sheet = await doc.addSheet({
+        title: desiredTitle,
+        headerValues: colNames,
+      });
+      await sheet.updateProperties({ index: 0 });
     }
 
     await sheet.addRow({
@@ -41,15 +88,19 @@ export const updateSheet = async ({ id, coach, type, fname, lname, location, ema
       Options: option.join(", "),
       InfoRequested: info.join(", "),
       ConvSource: heard.join(", "),
-      Date: today.toLocaleDateString('en-US', { timeZone: 'America/Los_Angeles' }),
-      Time: today.toLocaleTimeString('en-US', { timeZone: 'America/Los_Angeles' }),
+      Date: today.toLocaleDateString("en-US", {
+        timeZone: "America/Los_Angeles",
+      }),
+      Time: today.toLocaleTimeString("en-US", {
+        timeZone: "America/Los_Angeles",
+      }),
       ConvType: "Form",
       StageDesc: "Form Submission",
-      Stage: 0
+      Stage: 0,
     });
-    return true
+    return true;
   } catch (e) {
-    console.error("Error updating google sheet:", e)
+    console.error("Error updating google sheet:", e);
   }
-  return false
-}
+  return false;
+};
