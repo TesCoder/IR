87edcd257bd20bc46f6b99d618207f352b10909e
TesCoder <115577869+TesCoder@users.noreply.github.com>
2025-06-19 06:54:40 -0700

    Update membersList_h.js

--- next-js-site/components/membersList_h.js
+++ next-js-site/components/membersList_h.js
@@ -25,4 +25,17 @@ export const members_h = [
       description:
         "Heather Kannam has extensive experience in admissions offices and counseling.  In the past three years she has helped students who were admitted to top colleges across the US including Stanford, Yale, UPenn, UChicago, Columbia, Dartmouth, Brown, Cornell, Duke, MIT, NYU and Johns Hopkins.\nShe earned her MFA from the University of Iowa Playwrights’ Workshop and was a Senior Fellow in playwriting at Dartmouth College, where she graduated Phi Beta Kappa, magna cum laude.  She received a grant from the Kennedy Center’s Fund for New American Plays, a project in cooperation with the President’s Committee on the Arts and Humanities. Her fantasy novel LightLand, published by Scholastic/Orchard Books, earned a starred review in Publishers Weekly.  She is an internationally produced playwright and novelist, and the artistic director of a non-profit community theatre. Her own children have attended Dartmouth College, Harvard Graduate School of Education, and Boston University Law School. Her youngest was accepted at Harvard and Yale this year— the most competitive on record. Her son will attend Yale University with intention of studying Theatre!",
     },
+    {
+      id: "52547552",
+      fname: "James",
+      name: "James Dean",
+      imgSrc: "/images/members/james.png",
+      admCollege: "Stanford",
+      past: [
+        "B.S., Stanford University",
+        "Former Admission Officer, Stanford University",
+      ],
+      description:
+        "James brings wide and deep experience to assist students applying to US universities. Some of his experiences specifically involve Stanford: he got his engineering degree there, taught three design courses, served as small group coach for two other courses, did undergraduate admissions, had one daughter admitted and another waitlisted. But James has integrated the knowledge gained at Stanford with his other experiences teaching at both public and private high schools in the US, Australia, and New Zealand as well as being a parent of high achieving students. James understands the process and strives to work with motivated students in creating compelling “profiles” to generate a range of great admission offers.",
+    }
   ];
