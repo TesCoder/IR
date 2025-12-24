6002f0a3b6b67071c7d614480092aac5f4436dfe
TesCoder <115577869+TesCoder@users.noreply.github.com>
2025-12-24 10:10:08 -0800

    remove that Bootstrap CDN script from _app.js (not needed for navbar)

--- next-js-site/pages/_app.js
+++ next-js-site/pages/_app.js
@@ -64,13 +64,6 @@ function App({ Component, pageProps }) {
         `}
       </Script>
 
-      {/* Bootstrap Bundle */}
-      <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-        crossOrigin="anonymous"
-      />
-
       <Component {...pageProps} />
     </>
   );
